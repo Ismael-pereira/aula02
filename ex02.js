2670385fb92ac61368ba355f57a95204dbd978cd
Ismael-pereira <ismael.valdoni.pereira@escola.pr.gov.br>
2026-05-24 21:48:48 -0300

    ex02.js

--- ex02.js
+++ ex02.js
@@ -3,9 +3,9 @@ const validarSenhaForte = (senha) => {
   const regex = /^(?=.*[A-Z])(?=.*[0-9]).{6,}$/;
 
   if (regex.test(senha)) {
-    console.log("Senha forte");
+    console.log("Senha forte O");
   } else {
-    console.log("Senha fraca");
+    console.log("Senha fraca X");
   }
 
 };
